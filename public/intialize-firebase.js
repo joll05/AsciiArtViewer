aaa78305e40a54d004f0f5a66d31d3aff0adca3d
joll05 <44553291+joll05@users.noreply.github.com>
2020-06-18 23:55:05 +0200

    Started work

---
+++ public/intialize-firebase.js
@@ -0,0 +1,15 @@
+const { Firestore } = require("@google-cloud/firestore");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWP0DbqpmHyjMHutknHMKeTqMg0KChDc4",
+    authDomain: "ascii-art-viewer.firebaseapp.com",
+    databaseURL: "https://ascii-art-viewer.firebaseio.com",
+    projectId: "ascii-art-viewer",
+    storageBucket: "ascii-art-viewer.appspot.com",
+    messagingSenderId: "620614059691",
+    appId: "1:620614059691:web:907d10141053a7ef6a99ee"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
