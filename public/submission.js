f064964a17b6cbfcb5d5fee0131d52443da1e66a
joll05 <44553291+joll05@users.noreply.github.com>
2020-06-19 12:24:39 +0200

    Started on storage implementation

--- public/submission.js
+++ public/submission.js
@@ -1,3 +1,25 @@
-$("#submission-form").submit(function(event){
+let form = $("#submission-form");
+
+$(form).change(function(){
+    let submissionTypeValue = $(form).children("input[name='submission-type']:checked").val();
+    let isPasting = (submissionTypeValue == "paste");
     
+    $(".paste-area *").prop("required", isPasting);
+    $(".paste-area").toggle(isPasting);
+
+    $(".upload-area textarea").prop("required", !isPasting);
+    $(".upload-area").toggle(!isPasting);
 })
+
+$(form).submit(function(event){
+    event.preventDefault();
+
+    let data = {
+        submissionType : $("input[name='submission-type']:checked").val(),
+        submissionText : $("textarea[name='submission-text']").val(),
+        submissionFile : $("input[name='submission-file']").val(),
+        name : $("input[name='submission-name']").val()
+    };
+
+    console.log(data);
+});
